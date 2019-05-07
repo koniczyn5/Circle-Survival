e5819b45d877b04e648348e443ed1082e8e0b340
koniczyn5 <koniczyn5@tlen.pl>
2019-05-07 22:55:54 +0200

    First iteration of Menu

---
+++ Circle Survival/Assets/Scripts/MainMenuController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MainMenuController: MonoBehaviour
+{
+    private Text _highScoreText;
+    public void Start()
+    {
+        _highScoreText=GameObject.Find("HS_value_text").GetComponent<Text>();
+        _highScoreText.text=PlayerPrefs.GetFloat("HighScore", 0).ToString();
+    }
+
+    public void Play()
+    {
+        SceneManager.LoadScene("Game");
+    }
+    public void Exit()
+    {
+        Application.Quit();
+    }
+}
